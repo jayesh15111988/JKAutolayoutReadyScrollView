71dbc60f04bce66ea6df5ee6184494394db21610
Jayesh Kawli <j.kawli@gmail.com>
2015-07-17 22:07:59 -0400

    finished required sources. Added podspec file for easy integration through cocoapods

---
+++ JKAutolayoutReadyScrollView.podspec
@@ -0,0 +1,14 @@
+Pod::Spec.new do |s|
+  s.name         = "JKAutolayoutReadyScrollView"
+  s.version      = "0.0.1"
+  s.summary      = "A readymade scrollview equipped with Autolayout settings."
+  s.homepage     = "https://github.com/jayesh15111988/JKAutolayoutReadyScrollView"
+  s.license      = { :type => 'MIT', :file => 'LICENSE' }
+  s.author       = { "Jayesh Kawli" => "j.kawli@gmail.com" }
+  s.source       = { :git => "https://github.com/jayesh15111988/JKAutolayoutReadyScrollView.git", :tag => "#{s.version}" }
+  s.ios.deployment_target = '7.0'
+  s.source_files = 'JKAutolayoutReadyScrollView/Classes/*.{h,m}'
+  s.requires_arc = true
+  s.dependency 'TPKeyboardAvoiding', '~> 1.2'
+end
+
--- JKAutolayoutReadyScrollView/ViewController.m
+++ JKAutolayoutReadyScrollView/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "ScrollViewAutolayoutCreator.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    ScrollViewAutolayoutCreator* autoLayoutScrolView = [[ScrollViewAutolayoutCreator alloc] initWithSuperView:self.view];
+    UITextField* textF = [UITextField new];
+    textF.translatesAutoresizingMaskIntoConstraints = NO;
+    textF.borderStyle = UITextBorderStyleBezel;
+    textF.text = @"Autolayout Equipped Scrollview - Created by Jayesh Kawli";
+    textF.textAlignment = NSTextAlignmentCenter;
+    [autoLayoutScrolView.contentView addSubview:textF];
+    
+    UILabel* lab = [UILabel new];
+    lab.translatesAutoresizingMaskIntoConstraints = NO;
+    lab.text = @"Easily integrable into any app. No hassle to deal with ScrollView, contentView and ugly constraints to make scroll view work with the Auto layouts";
+    lab.textAlignment = NSTextAlignmentCenter;
+    lab.numberOfLines = 0;
+    [autoLayoutScrolView.contentView addSubview:lab];
+    
+    NSDictionary* views = NSDictionaryOfVariableBindings(textF, lab);
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-10-[textF]-10-|" options:kNilOptions metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-100-[textF]" options:kNilOptions metrics:nil views:views]];
+    
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-10-[lab]-10-|" options:kNilOptions metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[textF]-50-[lab]" options:kNilOptions metrics:nil views:views]];
+    
+    [autoLayoutScrolView addBottomSpaceConstraintToLastView:lab withBottomPadding:100];
 }
 
 - (void)didReceiveMemoryWarning {
